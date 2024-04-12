cefa47ff47bffdc4748055714b771f2bce5289ec
lifesteal <dorianosaure.pro@gmail.com>
2024-04-12 14:04:45 +0200

    First Commit

---
+++ src/main/java/fr/lifesteal/luxuryplant/LuxuryPlant.java
@@ -0,0 +1,33 @@
+package fr.lifesteal.luxuryplant;
+
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("luxuryplant")
+public class LuxuryPlant
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public LuxuryPlant() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+    }
+}
